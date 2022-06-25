3fd6b08fa3416389567bac6850c56df187afc527
XonotanSoft <108112356+XonotanSoft@users.noreply.github.com>
2022-06-24 19:47:17 -0500

    Update Load_Media.lua

--- Scripts/Load_Media.lua
+++ Scripts/Load_Media.lua
@@ -2,6 +2,6 @@ function load_media()
   local Sounds={}
   Sounds.colision = love.audio.newSource("Media/Sound/02.wav", "static")
   Sounds.click = love.audio.newSource("Media/Sound/01.wav", "static")
-  local cursor_s = love.graphics.newImage("Media/Images/cursor.png")
+  local cursor_s = love.graphics.newImage("Media/Image/cursor.png")
   return Sounds, cursor_s
 end
